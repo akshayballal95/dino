634e31e52e3939ba56d9335f423713d542ace343
= <=>
2023-05-12 09:44:57 +0530

    commit

--- dino.py
+++ dino.py
@@ -19,6 +19,7 @@ transformer = Compose([
     Normalize(mean =[0.485, 0.456, 0.406], std =[0.229, 0.224, 0.225])
 ])
 
+
 def generator():
     while(not keyboard.is_pressed("esc")):
       yield
--- process.py
+++ process.py
@@ -28,21 +28,3 @@ with open('labels_dino.csv', 'w') as csvfile:
     writer.writeheader()
     writer.writerows(labels)
 
-# #  read the csv file and plot the histogram of the classes
-# df = pd.read_csv("labels_dino.csv")
-# df.hist(column=["class"])
-
-# # balancing the dataset by sampling the non-action class to the same number of samples as the action class
-# action = df[df['class'] != 0]
-# non_action = df[df['class'] == 0]
-
-# num = action['class'].value_counts().max()
-# non_action = non_action.sample(n=num)
-
-# df_sample = pd.concat([action, non_action], axis=0).sample(frac=1)
-
-# df_sample.hist(column=['class'])
-
-# #write the balanced dataset to a csv file
-# df_sample.to_csv("labels.csv", index=False)
-# print(len(df_sample))
